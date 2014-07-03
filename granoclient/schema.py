3c870a34d0ee91dda6f7846945927e0dc472feff
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-03 14:58:43 +0200

    Ability to load schemata from a file.

--- granoclient/schema.py
+++ granoclient/schema.py
@@ -1,11 +1,16 @@
+import yaml
+import logging
 from granoclient.common import GranoResource, GranoCollection
+from granoclient.base import NotFound
+
+log = logging.getLogger()
 
 
 class Schema(GranoResource):
     """ A schema within grano. Schemata define the types of entities and
-    relations that are stored within a grano project. See also 
+    relations that are stored within a grano project. See also
     :ref:`schema`."""
-    
+
     resource_key = 'name'
 
     def __init__(self, client, base_endpoint, data):
@@ -33,7 +38,8 @@ class SchemaCollection(GranoCollection):
         """ Begin querying the collection. The query can further be refined
         using the methods of the returned :class:`granoclient.Query`."""
         clazz = lambda client, data: self.clazz(client, self.endpoint, data)
-        return self.query_clazz(self.client, clazz, self.endpoint, params=params)
+        return self.query_clazz(self.client, clazz, self.endpoint,
+                                params=params)
 
     def __init__(self, client, project_slug):
         self.project_slug = project_slug
@@ -49,9 +55,9 @@ class SchemaCollection(GranoCollection):
         return self.clazz(self.client, self.endpoint, data)
 
     def create(self, data):
-        """ Create a new schema. 
+        """ Create a new schema.
 
-        :param data: A dictionary with the schemas attributes, ``name`` 
+        :param data: A dictionary with the schemas attributes, ``name``
             and ``label`` are required, but several ``attributes`` should
             be given. See :ref:`schema` for details.
         """
@@ -59,3 +65,27 @@ class SchemaCollection(GranoCollection):
             data = data._data
         s, data = self.client.post(self.endpoint, data=data)
         return self.clazz(self.client, self.endpoint, data)
+
+    def upsert(self, data):
+        """ Import a schema from an object. This attempts to either create or
+        update schemata based on the specification given. """
+        name = data.get('name')
+        try:
+            schema = self.by_name(name)
+            schema._data = data
+            schema.save()
+            log.info('Updated schema: %s', schema.label)
+        except NotFound:
+            schema = self.create(data)
+            log.info('Created schema: %s', schema.label)
+
+
+    def upsert_from_file(self, file_name):
+        """ Same as ``from_list``, but works on a file name. """
+        # TODO: should this be a file object?
+        with open(file_name, 'rb') as fh:
+            data = yaml.load(fh)
+            if not isinstance(data, (list, tuple)):
+                data = [data]
+            for schema in data:
+                self.upsert(schema)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grano-client',
-    version='0.2.4',
+    version='0.3.0',
     description="Client library for grano, a social network analysis tool.",
     long_description=open('README.rst').read(),
     classifiers=[
@@ -16,7 +16,8 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     zip_safe=False,
     install_requires=[
-        "requests>=2.2.0"
+        "requests>=2.2.0",
+        "PyYAML==3.10"
     ],
     tests_require=[],
     entry_points=\
